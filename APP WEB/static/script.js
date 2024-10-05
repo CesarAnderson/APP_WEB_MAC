e2cfae94b9f86b96e85b902ef30dd0272376a7b2
CesarAnderson <cesandorepal@gmail.com>
2024-10-05 16:14:55 +0200

    prueba 1614

--- APP WEB/static/script.js
+++ APP WEB/static/script.js
@@ -1,4 +1,4 @@
-// script.jsprueba
+// script.js
 document.addEventListener('DOMContentLoaded', () => {
     const imagenInput = document.getElementById('imagenInput');
     const subirBtn = document.getElementById('subirBtn');
